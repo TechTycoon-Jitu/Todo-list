200e22a3aaba6d2d8b4c6bc6d31d445200f60cdd
Sanket <sanketmondal893@gmail.com>
2023-09-24 22:24:30 +0530

    first commit

---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List(){
+    return(
+        <ol>
+        <li>Vinod</li>
+        <li>Mondal</li>
+        <li>Thapa</li>
+        <li>Creative Thinkers</li>
+        <li>Please Like Share and Subscibe</li> 
+      </ol>
+    );
+}
+export default List;
